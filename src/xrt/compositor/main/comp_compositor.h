a9593c0d1ff3a2a13dafa12ba5c5aa8687c8ce46
Madhuparna Bhowmik <madhuparnabhowmik04@gmail.com>
2023-01-27 19:59:52 -0500

    Works fine with no changes in comp_Rendered except for illixr_Write.

--- src/xrt/compositor/main/comp_compositor.h
+++ src/xrt/compositor/main/comp_compositor.h
@@ -48,11 +48,11 @@ extern "C" {
  */
 enum comp_state
 {
-	COMP_STATE_UNINITIALIZED = 0,
-	COMP_STATE_READY = 1,
-	COMP_STATE_PREPARED = 2,
-	COMP_STATE_VISIBLE = 3,
-	COMP_STATE_FOCUSED = 4,
+    COMP_STATE_UNINITIALIZED = 0,
+    COMP_STATE_READY = 1,
+    COMP_STATE_PREPARED = 2,
+    COMP_STATE_VISIBLE = 3,
+    COMP_STATE_FOCUSED = 4,
 };
 
 
@@ -61,10 +61,10 @@ enum comp_state
  */
 struct comp_frame
 {
-	int64_t id;
-	uint64_t predicted_display_time_ns;
-	uint64_t desired_present_time_ns;
-	uint64_t present_slop_ns;
+    int64_t id;
+    uint64_t predicted_display_time_ns;
+    uint64_t desired_present_time_ns;
+    uint64_t present_slop_ns;
 };
 
 /*!
@@ -75,63 +75,63 @@ struct comp_frame
  */
 struct comp_compositor
 {
-	struct comp_base base;
+    struct comp_base base;
 
-	//! Renderer helper.
-	struct comp_renderer *r;
+    //! Renderer helper.
+    struct comp_renderer *r;
 
-	//! The target we are displaying to.
-	struct comp_target *target;
+    //! The target we are displaying to.
+    struct comp_target *target;
 
-	//! The device we are displaying to.
-	struct xrt_device *xdev;
+    //! The device we are displaying to.
+    struct xrt_device *xdev;
 
-	//! The settings.
-	struct comp_settings settings;
+    //! The settings.
+    struct comp_settings settings;
 
-	//! Vulkan shaders that the compositor uses.
-	struct render_shaders shaders;
+    //! Vulkan shaders that the compositor uses.
+    struct render_shaders shaders;
 
-	//! Timestamp of last-rendered (immersive) frame.
-	int64_t last_frame_time_ns;
+    //! Timestamp of last-rendered (immersive) frame.
+    int64_t last_frame_time_ns;
 
-	//! State for generating the correct set of events.
-	enum comp_state state;
+    //! State for generating the correct set of events.
+    enum comp_state state;
 
-	// Extents of one view, in pixels.
-	VkExtent2D view_extents;
+    // Extents of one view, in pixels.
+    VkExtent2D view_extents;
 
-	//! Are we mirroring any of the views to the debug gui? If so, turn off the fast path.
-	bool mirroring_to_debug_gui;
+    //! Are we mirroring any of the views to the debug gui? If so, turn off the fast path.
+    bool mirroring_to_debug_gui;
 
-	//! On screen window to display the content of the HMD.
-	struct comp_window_peek *peek;
+    //! On screen window to display the content of the HMD.
+    struct comp_window_peek *peek;
 
-	/*!
-	 * @brief Data exclusive to the begin_frame/end_frame for computing an
-	 * estimate of the app's needs.
-	 */
-	struct
-	{
-		int64_t last_begin;
-		int64_t last_end;
-	} app_profiling;
+    /*!
+     * @brief Data exclusive to the begin_frame/end_frame for computing an
+     * estimate of the app's needs.
+     */
+    struct
+    {
+        int64_t last_begin;
+        int64_t last_end;
+    } app_profiling;
 
-	struct u_frame_times_widget compositor_frame_times;
+    struct u_frame_times_widget compositor_frame_times;
 
-	struct
-	{
-		struct comp_frame waited;
-		struct comp_frame rendering;
-	} frame;
+    struct
+    {
+        struct comp_frame waited;
+        struct comp_frame rendering;
+    } frame;
 
-	struct
-	{
-		//! Temporarily disable ATW
-		bool atw_off;
-	} debug;
+    struct
+    {
+        //! Temporarily disable ATW
+        bool atw_off;
+    } debug;
 
-	struct render_resources nr;
+    struct render_resources nr;
 };
 
 
@@ -149,7 +149,7 @@ struct comp_compositor
 static inline struct comp_compositor *
 comp_compositor(struct xrt_compositor *xc)
 {
-	return (struct comp_compositor *)xc;
+    return (struct comp_compositor *)xc;
 }
 
 /*!
